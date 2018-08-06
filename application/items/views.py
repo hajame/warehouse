17a4b2d5439266bba63d2eb84ee8311926e09f56
Harri Mehtälä <harri.mehtala@helsinki.fi>
2018-08-06 13:16:34 +0300

    validation of information, error messages

--- application/items/views.py
+++ application/items/views.py
@@ -1,14 +1,18 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.items.models import Item
+from application.items.forms import ItemForm
+
 
 @app.route("/items/", methods=["GET"])
 def items_index():
-    return render_template("items/list.html", items = Item.query.all())
+    return render_template("items/list.html", items=Item.query.all())
+
 
 @app.route("/items/new/")
 def items_form():
-    return render_template("items/new.html")
+    return render_template("items/new.html", form=ItemForm())
+
 
 @app.route("/items/<item_id>/", methods=["POST"])
 def items_take_one(item_id):
@@ -17,8 +21,9 @@ def items_take_one(item_id):
     if t.amount > 0:
         t.amount = t.amount - 1
     db.session().commit()
-  
+
     return redirect(url_for("items_index"))
+
 
 @app.route("/items/<item_id>+/", methods=["POST"])
 def items_add_one(item_id):
@@ -26,12 +31,18 @@ def items_add_one(item_id):
     t = Item.query.get(item_id)
     t.amount = t.amount + 1
     db.session().commit()
-  
+
     return redirect(url_for("items_index"))
+
 
 @app.route("/items", methods=["POST"])
 def items_create():
-    item = Item(request.form.get("name"), request.form.get("volume"), request.form.get("amount"))
+    form = ItemForm(request.form)
+
+    if not form.validate():
+        return render_template("items/new.html", form = form)
+
+    item = Item(form.name.data, form.volume.data, form.amount.data)
     db.session().add(item)
     db.session().commit()
 
